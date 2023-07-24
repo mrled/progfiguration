bdc5ff4fffc97d5761635b5e5dc481a06d39becd
Micah R Ledbetter <me@micahrl.com>
2023-07-23 23:10:34 -0500

    Add ProgfigsitePythonPackagePreparer context manager

--- progfiguration/progfigbuild/__init__.py
+++ progfiguration/progfigbuild/__init__.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import pathlib
 import stat
 import textwrap
-from typing import Callable, Optional
+from typing import Callable, List, Optional
 import zipfile
 
 import progfiguration
@@ -17,15 +17,13 @@ from progfiguration.util import import_module_from_filepath
 
 @dataclass
 class InjectedFile:
-    """A file to inject into the zipapp
-
-    Properties:
-        path: The path to the file relative to the subpackage.
-        contents: The contents of the file.
-    """
+    """A file to inject into the built package"""
 
     path: pathlib.Path
+    """The path to the file relative to the package root."""
+
     contents: str
+    """The contents of the file."""
 
 
 def generate_builddata_version_py(version: str, build_date: datetime) -> str:
@@ -81,6 +79,10 @@ def build_progfigsite_zipapp(
 
     We don't use the zipapp code itself because we want more control over what goes into the zipfile,
     like ignoring __pycache__, *.dist-info, etc.
+
+    We don't need the ProgfigsitePythonPackagePreparer context manager here,
+    because we don't need to inject build data into the filesystem before building the zipapp.
+    We can inject the build data directly into the zipapp file.
     """
 
     if progfiguration_package_path is None:
@@ -169,12 +171,188 @@ def progfigsite_setuptools_builder(
     return result.stdout.read()
 
 
+class ProgfigsitePythonPackagePreparer:
+    """A context manager which prepares for building a Python package.
+
+    Preparation consists of finding the project path from the package path
+    (see `progfigsite_project_path` and `progfigsite_filesystem_path` properties),
+    and injecting build data into the filesystem
+    like a version from progfigsite's `mint_version()` and the build date.
+    When the context manager exits, the injected files are removed.
+
+    You can use this to build a pip package with build and setuptools like this:
+
+    ```python
+    with ProgfigsitePythonPackagePreparer("/path/to/progfigsite") as preparer:
+        result = subprocess.run(
+            ["python", "-m", "build", "-s", "-o", preparer.package_out_path.as_posix(), preparer.progfigsite_project_path.as_posix()],
+            check=True,
+            capture_output=True,
+            cwd=preparer.progfigsite_project_path,
+        )
+    built_package_path = result.stdout.read()
+    ```
+
+    See the implementation of `build_progfigsite_pip()` for a more complete example.
+
+    If you want to modify the build in some way,
+    or build another kind of package after we prepare the filesystem and inject build data,
+    you can do that instead.
+
+    Can be used by progfigsite to do something with the result of `python -m build`,
+    like build a package for another package manager.
+    """
+
+    def __init__(
+        self,
+        progfigsite_filesystem_path: pathlib.Path,
+        build_date: Optional[datetime] = None,
+        progfiguration_package_path: Optional[pathlib.Path] = None,
+        injections: Optional[List[InjectedFile]] = None,
+        keep_injected_files: bool = False,
+    ):
+        """
+        Args:
+            progfigsite_filesystem_path: The path to the progfigsite package, eg "/path/to/progfigsite/src".
+            build_date: The build date to inject into the package.
+                If None, the current date will be used.
+            progfiguration_package_path: The path to the progfiguration package, eg "/path/to/progfiguration".
+                If None, the progfiguration package will be copied from the Python path.
+                This will only work if progfiguration is installed to the Python path
+                (probably by pip in a venv, and possibly editable with 'pip install -e').
+            injections: Build data we will inject into the filesystem before building the pip package.
+                These injections can go anywhere in the filesystem --
+                they aren't limited to the progfigsite package.
+                You can inject files into the project directory (containing the pyproject.toml),
+                the package directory (containing the actual Python package),
+                or maybe even somewhere else entirely.
+            keep_injected_files: If True, the injected files will be kept after the package is built.
+                You will need to remove them manually. Intended for debugging.
+        """
+
+        self.progfigsite_filesystem_path = progfigsite_filesystem_path
+        """The filesystem path to the progfigsite package, eg `/path/to/progfigsite/src`
+
+        This PACKAGE directory should be the path to the Python package itself with an `__init__.py`.
+        It might be inside a subfolder like "src" or "progfigsite" inside the PROJECT directory,
+        which is the directory containing a pyproject.toml file.
+
+        See also: `progfigsite_project_path`.
+        """
+
+        self.progfiguration_package_path = progfiguration_package_path or pathlib.Path(progfiguration.__file__).parent
+        """The filesystem path to the progfiguration package, eg `/path/to/progfiguration`"""
+
+        self.build_date = build_date or datetime.utcnow()
+        """The build date to inject into the package version metadata"""
+
+        self.keep_injected_files = keep_injected_files
+        """If True, the injected files will be kept after the package is built."""
+
+        progfigsite, progfigsite_modname = import_module_from_filepath(progfigsite_filesystem_path)
+
+        self.minted_version: str = progfigsite.mint_version()
+        """The version of the package we are building, generated by a call to the progfigsite's `mint_version()`"""
+
+        builddata_version_py = generate_builddata_version_py(self.minted_version, self.build_date)
+
+        # Find the progfigsite PROJECT path containing a pyproject.toml
+        # progfigsite_filesystem_path might be inside a directory called 'src' or 'progfigsite' inside the PROJECT path
+        progfigsite_pyproject_toml_path = progfigsite_filesystem_path / "pyproject.toml"
+        while not progfigsite_pyproject_toml_path.exists():
+            if progfigsite_pyproject_toml_path == progfigsite_pyproject_toml_path.parent:
+                raise RuntimeError("Could not find progfigsite project path")
+            progfigsite_pyproject_toml_path = progfigsite_pyproject_toml_path.parent
+
+        self.progfigsite_project_path = progfigsite_pyproject_toml_path.parent
+        """The filesystem path to the progfigsite project, eg `/path/to/progfigsite`
+
+        This PROJECT directory should be the directory containing a pyproject.toml file.
+        It might contain a subfolder like "src" or "progfigsite" containing the Python PACKAGE itself.
+
+        This is the directory you should run `python -m build` from.
+
+        See also: `progfigsite_filesystem_path`.
+        """
+
+        self.progfiguration_autovendored_path = progfigsite_filesystem_path / "autovendor" / "progfiguration"
+        """The filesystem path to the progfiguration package inside the progfigsite package
+
+        We inject a symlink to the progfiguration package into the progfigsite package
+        so that progfiguration can find it when run from the pip package.
+        (`python -m build` will dereference the symlink and include the actual progfiguration package in the pip package.)
+        """
+
+        self.injections = injections or []
+        """Build data we will inject into the filesystem before building the pip package
+
+        We always inject a version.py file containing the version and build date.
+        We have to do this on disk before building the pip package;
+        we can't inject version metadata into the package after it's built,
+        because we need to know the version before we build the package.
+
+        Users may also inject other files if they want to with the `injections` argument.
+        """
+        self.injections += [
+            InjectedFile(
+                path=progfigsite_filesystem_path / "builddata" / "version.py",
+                contents=builddata_version_py,
+            ),
+        ]
+
+        self.failed_unlinks: List[str] = []
+
+    def __enter__(self):
+
+        # Inject build data into the packages
+        logger.debug(f"Injecting progfiguration symlink {self.progfiguration_autovendored_path}...")
+        if self.progfiguration_autovendored_path.exists():
+            self.progfiguration_autovendored_path.unlink()
+        self.progfiguration_autovendored_path.symlink_to(self.progfiguration_package_path)
+        for injection in self.injections:
+            with injection.path.open("w") as fp:
+                logger.debug(f"Injecting {injection.path}...")
+                fp.write(injection.contents)
+
+        # Return self so we can be used as a context manager
+        return self
+
+    def __exit__(self, type, value, traceback):
+
+        # Clean up the injected files
+        if self.keep_injected_files:
+            logger.warning("Keeping injected files for debugging...")
+            logger.debug(f"- {self.progfiguration_autovendored_path}")
+            for injection in self.injections:
+                logger.debug(f"- {injection.path}")
+        else:
+            try:
+                self.progfiguration_autovendored_path.unlink()
+                logger.debug(f"Unlinked progfiguration symlink {self.progfiguration_autovendored_path}...")
+            except Exception:
+                self.failed_unlinks.append(self.progfiguration_autovendored_path)
+                logger.debug(f"Failed to unlink progfiguration symlink {self.progfiguration_autovendored_path}...")
+            for injection in self.injections:
+                try:
+                    injection.path.unlink()
+                    logger.debug(f"Unlinked {injection.path}...")
+                except Exception:
+                    self.failed_unlinks.append(injection.path)
+                    logger.debug(f"Failed to unlink {injection.path}...")
+
+        # If we were unable to clean up the injected files, notify the user with an error
+        if self.failed_unlinks:
+            msg = ["Failed to unlink the following files, make sure not to commit them to version control:"]
+            for path in self.failed_unlinks:
+                msg.append(f"  {path}")
+            raise RuntimeError("\n".join(msg))
+
+
 def build_progfigsite_pip(
     progfigsite_filesystem_path: pathlib.Path,
     package_out_path: pathlib.Path,
     build_date: Optional[datetime] = None,
     progfiguration_package_path: Optional[pathlib.Path] = None,
-    builder_function: Callable = progfigsite_setuptools_builder,
     keep_injected_files: bool = False,
 ) -> pathlib.Path:
     """Build a pip package
@@ -189,10 +367,6 @@ def build_progfigsite_pip(
             This will only work if progfiguration is installed via pip
             (probably in a venv, possibly editable with 'pip install -e')...
             it won't work if you're running progfiguration itself from a zipapp for some reason.
-        builder_function: A function which builds the pip package.
-            See the `progfigsite_setuptools_builder()` function for what arguments it should take.
-            TODO: Improve builder_function documentation.
-            You can supply your own builder function if you prefer to build pip packages via some other method.
         keep_injected_files: If True, the injected files will be kept after the package is built.
             You will need to remove them manually.
             Intended for debugging.
@@ -201,82 +375,21 @@ def build_progfigsite_pip(
         The path to the pip package, eg "/path/to/my_progfigsite/dist/my_progfigsite-0.1.0.tar.gz"
     """
 
-    if progfiguration_package_path is None:
-        progfiguration_package_path = pathlib.Path(progfiguration.__file__).parent
-
-    if build_date is None:
-        build_date = datetime.utcnow()
-
-    package_out_path.mkdir(parents=True, exist_ok=True)
-
-    progfigsite, progfigsite_modname = import_module_from_filepath(progfigsite_filesystem_path)
-    version = progfigsite.mint_version()
-    builddata_version_py = generate_builddata_version_py(version, build_date)
-
-    # Find the progfigsite PROJECT path containing a pyproject.toml
-    # progfigsite_filesystem_path might be inside a directory called 'src' or 'progfigsite' inside the PROJECT path
-    progfigsite_pyproject_toml_path = progfigsite_filesystem_path / "pyproject.toml"
-    while not progfigsite_pyproject_toml_path.exists():
-        if progfigsite_pyproject_toml_path == progfigsite_pyproject_toml_path.parent:
-            raise RuntimeError("Could not find progfigsite project path")
-        progfigsite_pyproject_toml_path = progfigsite_pyproject_toml_path.parent
-    progfigsite_project_path = progfigsite_pyproject_toml_path.parent
-
-    progfiguration_autovendored_path = progfigsite_filesystem_path / "autovendor" / "progfiguration"
-
-    # Inject build data into the packages.
-    # We have to do this on disk before building the pip package.
-    injections = [
-        InjectedFile(
-            path=progfigsite_filesystem_path / "builddata" / "version.py",
-            contents=builddata_version_py,
-        ),
-    ]
-
-    # We wrap our call to build the pip package in a try/finally block
-    # which ensures we don't leave any of our injected files on disk whether the build succeeds or fails.
-    failed_unlinks = []
-    try:
-        # Inject build data into the packages
-        logger.debug(f"Injecting progfiguration symlink {progfiguration_autovendored_path}...")
-        if progfiguration_autovendored_path.exists():
-            progfiguration_autovendored_path.unlink()
-        progfiguration_autovendored_path.symlink_to(progfiguration_package_path)
-        for injection in injections:
-            with injection.path.open("w") as fp:
-                logger.debug(f"Injecting {injection.path}...")
-                fp.write(injection.contents)
-
-        # Build the pip package
-        built = builder_function(progfigsite_project_path, package_out_path)
-
-    finally:
-        # Clean up the injected files
-        if keep_injected_files:
-            logger.warning("Keeping injected files for debugging...")
-            logger.debug(f"- {progfiguration_autovendored_path}")
-            for injection in injections:
-                logger.debug(f"- {injection.path}")
-        else:
-            try:
-                progfiguration_autovendored_path.unlink()
-                logger.debug(f"Unlinked progfiguration symlink {progfiguration_autovendored_path}...")
-            except Exception:
-                failed_unlinks.append(progfiguration_autovendored_path)
-                logger.debug(f"Failed to unlink progfiguration symlink {progfiguration_autovendored_path}...")
-            for injection in injections:
-                try:
-                    injection.path.unlink()
-                    logger.debug(f"Unlinked {injection.path}...")
-                except Exception:
-                    failed_unlinks.append(injection.path)
-                    logger.debug(f"Failed to unlink {injection.path}...")
-
-    # If we were unable to clean up the injected files, notify the user with an error
-    if failed_unlinks:
-        msg = ["Failed to unlink the following files, make sure not to commit them to version control:"]
-        for path in failed_unlinks:
-            msg.append(f"  {path}")
-        raise RuntimeError("\n".join(msg))
-
-    return built
+    with ProgfigsitePythonPackagePreparer(
+        progfigsite_filesystem_path,
+        build_date=build_date,
+        progfiguration_package_path=progfiguration_package_path,
+        keep_injected_files=keep_injected_files,
+    ) as preparer:
+        # built = builder_function(preparer.progfigsite_project_path, package_out_path)
+        cmd = [
+            "python",
+            "-m",
+            "build",
+            "-s",
+            "-o",
+            package_out_path.as_posix(),
+            preparer.progfigsite_package_path.as_posix(),
+        ]
+        result = magicrun(cmd)
+    return pathlib.Path(result.stdout.read())
