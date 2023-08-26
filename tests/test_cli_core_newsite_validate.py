74a1fed2b0650a6d4637de6ea31196f710b4d511
Micah R Ledbetter <me@micahrl.com>
2023-08-26 00:03:38 -0500

    Add test for installing site as editable in a clean venv

--- tests/test_cli_core_newsite_validate.py
+++ tests/test_cli_core_newsite_validate.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 import shutil
-import sys
 import tempfile
+import textwrap
 import unittest
 import venv
 
 import progfiguration
-from progfiguration import sitewrapper
 
 try:
     import tomllib
@@ -113,7 +112,9 @@ class TestRun(unittest.TestCase):
 
         # Use the venv python, which is guaranteed not to have progfiguration core or any site installed,
         # to run the newly created zipapp, and validate.
-        result = cmd.magicrun([venv_python.as_posix(), pyzfile.as_posix(), "validate"], print_output=verbose_test_output())
+        result = cmd.magicrun(
+            [venv_python.as_posix(), pyzfile.as_posix(), "validate"], print_output=verbose_test_output()
+        )
         stdout = result.stdout.read()
         self.assertTrue(result.returncode == 0)
         self.assertRegex(stdout, r"Progfigsite \(Python path: '.*'\) is valid.")
@@ -178,3 +179,113 @@ class TestRun(unittest.TestCase):
         validate_stdout = validate_result.stdout.read()
         self.assertEqual(validate_result.returncode, 0)
         self.assertEqual(validate_stdout, f"Progfigsite (Python path: '{self.sitename}') is valid.\n")
+
+    @pdbexc
+    @skipUnlessAnyEnv(["PROGFIGURATION_TEST_SLOW_ALL", "PROGFIGURATION_TEST_SLOW_SITE"])
+    def test_site_pip_install_editable_development_extras(self):
+        """Test that the site is installable as "pip install -e '.[developmenmt]'".
+
+        This is a slow test, as it creates a virtualenv,
+        and installs progfiguration core into it.
+
+        Test that the site can be installed as editable
+        without progfiguration core installed first.
+        Pip installing the '.[development]' extra will install the progfiguration core,
+        but first pip will find the version of the site package.
+        If finding the version depends on progfiguration core,
+        then the test fails.
+        Make sure we haven't accidentally required that progfiguration core be installed
+        before we can install any site in a venv as editable.
+        """
+
+        sitename = "test_spiede"
+        venvdir = self.tmpdir / f"venv_{sitename}"
+
+        # Make a new site, dedicated to this one test
+        projectdir = self.tmpdir / sitename
+        packagedir = projectdir / sitename
+        controllerage = self.tmpdir / f"controller.{sitename}.age"
+        newsite_result = cmd.magicrun(
+            [
+                "progfiguration",
+                "newsite",
+                "--name",
+                sitename,
+                "--path",
+                projectdir.as_posix(),
+                "--controller-age-key-path",
+                controllerage.as_posix(),
+            ],
+            print_output=verbose_test_output(),
+        )
+        self.assertTrue(newsite_result.returncode == 0)
+
+        # Modify pyproject.toml to install progfiguration core from the local filesystem.
+        # By default, 'progfiguration newsite' sets the 'development' extra
+        # to install progfiguration core form PyPI.
+        # Install from local instead, because:
+        # 1. It's faster
+        # 2. We can test local changes to progfiguration core
+        progfiguration_proj_path = Path(progfiguration.__file__).parent.parent
+        pyproject_toml = projectdir / "pyproject.toml"
+        with pyproject_toml.open("w") as fp:
+            fp.write(
+                textwrap.dedent(
+                    f"""\
+                        [project]
+                        name = "{sitename}"
+                        dynamic = ["version"]
+                        description = "Test project"
+                        [project.scripts]
+                        {sitename} = "{sitename}.cli.progfigsite_shim:main"
+                        [project.optional-dependencies]
+                        development = ["progfiguration @ file://{progfiguration_proj_path}"]
+                        [build-system]
+                        requires = ["setuptools"]
+                        build-backend = "setuptools.build_meta"
+                        [tool.setuptools.package-data]
+                        example_site = ["*"]
+                        [tool.setuptools.dynamic.version]
+                        attr = "{sitename}.version.get_version"
+                        [tool.black]
+                        line-length = 120
+                    """
+                )
+            )
+        if verbose_test_output():
+            print("Contents of pyproject.toml:")
+            print(pyproject_toml.read_text())
+
+        # Create a venv
+        # Add pip (slow), so we can install the site and its core dependency into the venv
+        venv.create(venvdir.as_posix(), with_pip=True)
+        venv_python = venvdir / "bin" / "python"
+
+        # Install the site into the venv as editable
+        pip_site_result = cmd.magicrun(
+            [
+                venv_python.as_posix(),
+                "-m",
+                "pip",
+                "--disable-pip-version-check",
+                "install",
+                "--editable",
+                ".[development]",
+            ],
+            cwd=projectdir.as_posix(),
+            print_output=verbose_test_output(),
+        )
+        self.assertTrue(pip_site_result.returncode == 0)
+
+        # Validate the site from within the venv
+        venv_site_cmd = venvdir / "bin" / sitename
+        validate_result = cmd.magicrun(
+            [
+                venv_site_cmd.as_posix(),
+                "validate",
+            ],
+            print_output=verbose_test_output(),
+        )
+        validate_stdout = validate_result.stdout.read()
+        self.assertEqual(validate_result.returncode, 0)
+        self.assertEqual(validate_stdout, f"Progfigsite (Python path: '{sitename}') is valid.\n")
