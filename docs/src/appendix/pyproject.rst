30d3b9fccdaba86cd48563e74044ab6b3a9d4f2c
Micah R Ledbetter <me@micahrl.com>
2023-09-03 22:50:04 -0500

    Fix missing package-data once and for all
    Fix broken test_site_pip_install_editable_packaging_extras()

--- docs/src/appendix/pyproject.rst
+++ docs/src/appendix/pyproject.rst
@@ -3,6 +3,17 @@ pyproject.toml edge cases
 
 We found some edge cases and surprising behavior with pyproject.toml.
 
+Default ``pyproject.toml``
+--------------------------
+
+When creating a new site with ``progfiguration newsite ...``,
+it creates a ``pyproject.toml`` for you that looks like the following.
+(Substitute a site name like ``mysite`` for ``{$}name``,
+and some reasonable description like ``the example.org site`` for ``{$}descriptiono``.)
+
+..  literalinclude:: ../../../progfiguration/newsite/pyproject.toml.temple
+    :language: toml
+
 Dynamic version
 ---------------
 
@@ -11,9 +22,7 @@ We use
 ::
 
     [tool.setuptools.dynamic]
-    version = {attr = "progfigsite.version.get_version"}
-
-(Substitute your package name for ``progfigsite``.)
+    version = {attr = "SITENAME.version.get_version"}
 
 This looks in the :doc:`/user-reference/progfigsite/version`
 for a function called ``get_version()``.
@@ -23,14 +32,14 @@ It runs that function
 2.  when building the package with ``python -m build ...``
     (which is used under the hood when running ``progfiguration build ...``).
 
-It finds this function inside the progfigsite package,
+It finds this function inside the SITENAME package,
 which means we must talk about package discovery.
 
 Discoveries about package discovery
 -----------------------------------
 
 tl;dr: use `src-layout <https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#src-layout>`_
-and everything will work implicitly.
+and everything will work easily.
 
 src-layout is the path of least resistance when using pip editable installs
 and package builds with setuptools.
@@ -41,12 +50,13 @@ and package builds with setuptools.
     ├── pyproject.toml
     ├── ...
     └── src/
-        └── progfigsite/
+        └── SITENAME/
             ├── __init__.py
             ├── ...
 
-With this layout, setuptools will find the ``progfigsite`` package automatically,
-and will include all non-Python data files like ``inventory.conf`` (so-called "package data") implicitly.
+With this layout, setuptools will find the ``SITENAME`` package automatically,
+and will include all non-Python data files like ``inventory.conf``
+(so-called "package data") via our ``[tool.setuptools.package-data]`` section.
 
 Problems using flat layout
 ^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -58,20 +68,21 @@ Flat layout is similar to src-layout, but without the "src" directory.
     progfigsite_root_directory
     ├── pyproject.toml
     ├── ...
-    └── progfigsite/
+    └── SITENAME/
         ├── __init__.py
         ├── ...
 
 We ran into problems with flat layout and do not recommend it for progfigsite packages.
 Here are some very rough notes.
 
-* We had to provide a ``[tool.setuptools.packages.find]`` section in ``pyproject.toml``,
-  although I don't remember why
+* We had to provide a ``[tool.setuptools.packages.find]`` section in ``pyproject.toml``.
 * When we did ``where = ["."]`` in that section,
   it seemed to work, but when trying to retrieve the version,
-  it couldn't find the ``progfigsite`` module
-  (``ModuleNotFoundError: No module named 'progfigsite'``).
-* We had to do ``where = ["progfigsite"]`` to get it to find the ``progfigsite`` module.
+  it couldn't find the ``SITENAME`` module
+  (``ModuleNotFoundError: No module named 'SITENAME'``).
+* We had to do ``where = ["SITENAME"]`` to get it to find the ``SITENAME`` module.
 * We also had trouble with package data,
   where non-Python files like secrets JSON files or ``inventory.conf`` files
   would not get included in packages built by setuptools.
+  In the ``[tool.setuptools.package-data]`` section,
+  we tried both ``SITENAME = ["*"]`` and ``"*" = ["*"]`` with no success.
--- tests/test_cli_core_newsite_pip_install_editable_development_extras.py
+++ tests/test_cli_core_newsite_pip_install_editable_development_extras.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import shutil
-import string
+import tarfile
 import tempfile
 import unittest
 import venv
+import zipfile
 
 import progfiguration
 
@@ -12,61 +13,23 @@ from progfiguration import cmd
 from tests import pdbexc, skipUnlessAnyEnv, verbose_test_output
 
 
-_pyproject_toml_template = string.Template(
-    """\
-[project]
-name = "$sitename"
-dynamic = ["version"]
-description = "Test project"
-[project.scripts]
-$sitename = "$sitename.cli.progfigsite_shim:main"
-[project.optional-dependencies]
-development = ["progfiguration @ file://$progfiguration_proj_path"]
-[build-system]
-requires = ["setuptools"]
-build-backend = "setuptools.build_meta"
-[tool.setuptools.dynamic.version]
-attr = "$sitename.version.get_version"
-[tool.black]
-line-length = 120
-"""
-)
-"""Template for pyproject.toml files created by tests
+def list_tgz(tarball: str) -> list[str]:
+    """List the contents of a tarball"""
+    with tarfile.open(tarball, "r:gz") as tar:
+        return [member.name for member in tar.getmembers()]
 
-* Use the sitename as the package and script names.
-* Use the local filesystem path to progfiguration core.
-  This is much faster and more reliable for tests than installing from PyPI,
-  and allows us to test local changes to progfiguration core.
-"""
+
+def list_whl(whl: str) -> list[str]:
+    """List the contents of a wheel (zip file)"""
+    with zipfile.ZipFile(whl, "r") as z:
+        return [member.filename for member in z.infolist()]
 
 
 class TestRun(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        """Set up a temporary directory and create the newsite"""
+        """Set up a temporary directory"""
         cls.tmpdir = Path(tempfile.mkdtemp())
-        cls.sitename = "testsite_xyz"
-        cls.projectdir = cls.tmpdir / "testproj"
-        cls.packagedir = cls.projectdir / "src" / cls.sitename
-        cls.controllerage = cls.tmpdir / "controller.age"
-        cls.pyproject = cls.projectdir / "pyproject.toml"
-
-        # Create the new site in the temp directory
-        cls.newsite_result = cmd.magicrun(
-            [
-                "progfiguration",
-                "newsite",
-                "--name",
-                cls.sitename,
-                "--path",
-                cls.projectdir.as_posix(),
-                "--controller-age-key-path",
-                cls.controllerage.as_posix(),
-            ],
-            print_output=False,
-        )
-
-        # cmd.magicrun(["find", cls.tmpdir])
 
     @classmethod
     def tearDownClass(cls):
@@ -75,190 +38,192 @@ class TestRun(unittest.TestCase):
 
     @pdbexc
     @skipUnlessAnyEnv(["PROGFIGURATION_TEST_SLOW_ALL", "PROGFIGURATION_TEST_SLOW_SITE"])
-    def test_site_pip_install_editable_development_extras(self):
-        """Test that the site is installable as "pip install -e '.[developmenmt]'".
-
-        This is a slow test, as it creates a virtualenv,
-        and installs progfiguration core into it.
-
-        Test that the site can be installed as editable
-        without progfiguration core installed first.
-        Pip installing the '.[development]' extra will install the progfiguration core,
-        but first pip will find the version of the site package.
-        If finding the version depends on progfiguration core,
-        then the test fails.
-        Make sure we haven't accidentally required that progfiguration core be installed
-        before we can install any site in a venv as editable.
-        """
-
-        sitename = "test_spiede"
-        venvdir = self.tmpdir / f"venv_{sitename}"
-
-        # Make a new site, dedicated to this one test
-        projectdir = self.tmpdir / sitename
-        packagedir = projectdir / sitename
-        controllerage = self.tmpdir / f"controller.{sitename}.age"
-        newsite_result = cmd.magicrun(
-            [
-                "progfiguration",
-                "newsite",
-                "--name",
-                sitename,
-                "--path",
-                projectdir.as_posix(),
-                "--controller-age-key-path",
-                controllerage.as_posix(),
-            ],
-            print_output=verbose_test_output(),
-        )
-        self.assertTrue(newsite_result.returncode == 0)
-
-        # Modify pyproject.toml to install progfiguration core from the local filesystem.
-        progfiguration_proj_path = Path(progfiguration.__file__).parent.parent
-        pyproject_toml = projectdir / "pyproject.toml"
-        with pyproject_toml.open("w") as fp:
-            fp.write(
-                _pyproject_toml_template.substitute(
-                    sitename=sitename,
-                    progfiguration_proj_path=progfiguration_proj_path,
-                )
-            )
-        if verbose_test_output():
-            print("Contents of pyproject.toml:")
-            print(pyproject_toml.read_text())
-
-        # Create a venv
-        # Add pip (slow), so we can install the site and its core dependency into the venv
-        venv.create(venvdir.as_posix(), with_pip=True)
-        venv_python = venvdir / "bin" / "python"
-
-        # Install the site into the venv as editable
-        pip_site_result = cmd.magicrun(
-            [
-                venv_python.as_posix(),
-                "-m",
-                "pip",
-                "--disable-pip-version-check",
-                "install",
-                "--editable",
-                ".[development]",
-            ],
-            cwd=projectdir.as_posix(),
-            print_output=verbose_test_output(),
-        )
-        self.assertTrue(pip_site_result.returncode == 0)
-
-        # Validate the site from within the venv
-        venv_site_cmd = venvdir / "bin" / sitename
-        validate_result = cmd.magicrun(
-            [
-                venv_site_cmd.as_posix(),
-                "validate",
-            ],
-            print_output=verbose_test_output(),
-        )
-        validate_stdout = validate_result.stdout.read()
-        self.assertEqual(validate_result.returncode, 0)
-        self.assertEqual(validate_stdout, f"Progfigsite (Python path: '{sitename}') is valid.\n")
-
-    @pdbexc
-    # @skipUnlessAnyEnv(["PROGFIGURATION_TEST_SLOW_ALL", "PROGFIGURATION_TEST_SLOW_SITE"])
-    @unittest.skip("Broken until we implement src layout")
-    def test_site_pip_install_build_install(self):
+    def test_site_pip_install_editable_packaging_extras(self):
         """Test that the site can be built and installed with pip.
 
         This is a slow test, as it creates a fresh virtualenv
         to install the site package into.
         """
 
-        sitename = "test_spibi"
-        venvdir = self.tmpdir / f"venv_{sitename}"
+        sitename = "test_spiepe"
+        venvdir_inst = self.tmpdir / f"venv_inst_{sitename}"
+        venvdir_build = self.tmpdir / f"venv_build_{sitename}"
+
+        # Create a venv for building the site
+        # Add pip (slow), so we can install the site into it
+        venv.create(venvdir_build.as_posix(), with_pip=True)
+
+        # Install progfiguration core into the venv.
+        # Use its path and install it as editable.
+        # Doing this here means that when setuptools encounters progfiguration
+        # as a dependency in the site's pyproject.toml,
+        # it will find it in the venv and use that version.
+        # This lets us test the site against the local progfiguration core
+        # (which may have changes that we need to test),
+        # and is also faster than installing over the network.
+        #
+        # The parent of the progfiguration core package is the directory containing pyproject.toml
+        progfiguration_core_project_path = Path(progfiguration.__file__).parent.parent
+        cmd.magicrun(
+            [
+                venvdir_build / "bin" / "pip",
+                "--disable-pip-version-check",
+                "install",
+                "--editable",
+                progfiguration_core_project_path,
+            ],
+            print_output=verbose_test_output(),
+        )
 
         # Make a new site, dedicated to this one test
         projectdir = self.tmpdir / sitename
-        packagedir = projectdir / sitename
+        packagedir = projectdir / "src" / sitename
         controllerage = self.tmpdir / f"controller.{sitename}.age"
         newsite_result = cmd.magicrun(
             [
-                "progfiguration",
+                venvdir_build / "bin" / "progfiguration",
                 "newsite",
                 "--name",
                 sitename,
                 "--path",
-                projectdir.as_posix(),
+                projectdir,
                 "--controller-age-key-path",
-                controllerage.as_posix(),
+                controllerage,
             ],
             print_output=verbose_test_output(),
         )
         self.assertTrue(newsite_result.returncode == 0)
 
-        # Modify pyproject.toml to install progfiguration core from the local filesystem.
-        progfiguration_proj_path = Path(progfiguration.__file__).parent.parent
-        pyproject_toml = projectdir / "pyproject.toml"
-        with pyproject_toml.open("w") as fp:
-            fp.write(
-                _pyproject_toml_template.substitute(
-                    sitename=sitename,
-                    progfiguration_proj_path=progfiguration_proj_path,
-                )
-            )
+        # Print the contents of pyproject.toml for debugging
+        if verbose_test_output():
+            pyproject_toml = projectdir / "pyproject.toml"
+            print("Contents of pyproject.toml:")
+            print(pyproject_toml.read_text())
+
+        # Install the new site into the venv as editable
+        #
+        # We have to do this because we use a dynamic version in pyproject.toml,
+        # and the site has to be importable into the same Python process
+        # that runs the build command for this to work,
+        # otherwise it will fail with error messages like:
+        #   ModuleNotFoundError: test_spibi/version
+        #
+        # It's also a convenient way to get the site's packaging extras installed.
+        site_install_result = cmd.magicrun(
+            [
+                venvdir_build / "bin" / "pip",
+                "--disable-pip-version-check",
+                "install",
+                "--editable",
+                ".[packaging]",
+            ],
+            cwd=projectdir.as_posix(),
+            print_output=verbose_test_output(),
+        )
 
         # Build the site into a pip package
         pip_build = self.tmpdir / "pip_build"
         prog_build_result = cmd.magicrun(
             [
-                "progfiguration",
+                venvdir_build / "bin" / "progfiguration",
                 "build",
                 "--progfigsite-fspath",
-                packagedir.as_posix(),
+                packagedir,
                 "--progfigsite-modname",
                 sitename,
                 "pip",
                 "--outdir",
-                pip_build.as_posix(),
+                pip_build,
             ],
+            cwd=projectdir.as_posix(),
             print_output=verbose_test_output(),
         )
         self.assertTrue(prog_build_result.returncode == 0)
 
         # Find the pip package in the build directory
-        pip_package = None
+        pip_src_pkg = None
+        pip_whl_pkg = None
         for child in pip_build.iterdir():
             if verbose_test_output():
-                print(f"{pip_build} / {child}")
-            if child.name.endswith(".tar.gz"):
-                pip_package = child
-                break
-        if pip_package is None:
-            raise RuntimeError(f"Could not find pip package in {pip_build}")
+                print(f"test_site_pip_install_build_install: Found built pip package: {pip_build} / {child}")
+            if child.name.endswith(".tar.gz") and pip_src_pkg is None:
+                pip_src_pkg = child
+            elif child.name.endswith(".whl") and pip_whl_pkg is None:
+                pip_whl_pkg = child
+            else:
+                raise RuntimeError(f"Unexpected file in pip build directory: {child}")
+        if pip_src_pkg is None:
+            raise RuntimeError(f"Could not find pip source package in {pip_build}")
+        # if pip_whl_pkg is None:
+        #     raise RuntimeError(f"Could not find pip binary package in {pip_build}")
 
-        # Create a venv
+        pkgvers = str(pip_src_pkg).replace(".tar.gz", "").split("-")[-1]
+        pip_src_files = list_tgz(pip_src_pkg)
+        # pip_whl_files = list_whl(pip_whl_pkg)
+
+        if verbose_test_output():
+            msg = "Built pip packages:"
+            msg += f"\n  List of files in build output directory at {pip_build}:"
+            msg += "\n  - " + "\n  - ".join([child.name for child in pip_build.iterdir()])
+            msg += f"\n  Full list of contents for source package at {pip_src_pkg}:"
+            msg += "\n  - " + "\n  - ".join(pip_src_files)
+            # msg += f"\n  Full list of contents for binary package at {pip_whl_pkg}:"
+            # msg += "\n  - " + "\n  - ".join(pip_whl_files)
+            msg += f"\n  Detected version as {pkgvers}"
+            msg += "\n"
+            print(msg)
+
+        # Examine the pip packages
+        # Make sure it includes what we expect,
+        # especially non-Python files like inventory.conf which are not included by default.
+        # Note that the wheel won't include things pyproject.toml,
+        # and that the source package will place all files in a subdirectory
+        # named after the package and version.
+        required_files = [
+            f"__init__.py",
+            f"inventory.conf",
+            f"builddata/static_include/progfiguration/__init__.py",
+        ]
+        missing_files = []
+        for reqfile in required_files:
+            # The pip src package has all filenames prefixed with the package name and version,
+            # but the wheel does not.
+            # if f"{sitename}-{pkgvers}/{reqfile}" not in pip_src_files or reqfile not in pip_whl_files:
+            #     missing_files.append(reqfile)
+            if f"{sitename}-{pkgvers}/src/{sitename}/{reqfile}" not in pip_src_files:
+                missing_files.append(reqfile)
+        if missing_files:
+            setuptools_version = cmd.magicrun(
+                [venvdir_build / "bin" / "python", "-c", "import setuptools; print(setuptools.__version__)"]
+            ).stdout.read()
+            msg += f"\nSetuptools inside the venv is version {setuptools_version}"
+            raise RuntimeError(f"Spot checking for required files, but at least some are missing: {missing_files}")
+
+        # Make sure the version is not the fallback version,
+        # which would indicate that the version was not injected into the builddata.
+        self.assertTrue(pkgvers != "0.0.1a0")
+
+        # Create a venv to install the package to
         # Add pip (slow), so we can install the site into it
-        venv.create(venvdir.as_posix(), with_pip=True)
-        venv_python = venvdir / "bin" / "python"
+        venv.create(venvdir_inst.as_posix(), with_pip=True)
 
         # Install the site into the venv as editable
         pip_site_result = cmd.magicrun(
             [
-                venv_python.as_posix(),
+                venvdir_inst / "bin" / "python",
                 "-m",
                 "pip",
                 "--disable-pip-version-check",
                 "install",
-                pip_package.as_posix(),
+                pip_src_pkg,
             ],
             print_output=verbose_test_output(),
         )
         self.assertTrue(pip_site_result.returncode == 0)
 
         # Validate the site from within the venv
-        venv_site_cmd = venvdir / "bin" / sitename
         validate_result = cmd.magicrun(
-            [
-                venv_site_cmd.as_posix(),
-                "validate",
-            ],
+            [venvdir_inst / "bin" / sitename, "validate"],
             print_output=verbose_test_output(),
         )
         validate_stdout = validate_result.stdout.read()
